9e0f4266be9a749c14678e4860cf5cd40d7fa488
ArjunPrajapati7824 <prajapatiak585@gmail.com>
2024-02-23 18:36:24 +0530

    backend .Net

---
+++ Models/PaymentDetailContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentDetailsCrudWeb.Models
+{
+    public class PaymentDetailContext:DbContext
+
+    {
+        public PaymentDetailContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+
+
+    }
+} 
